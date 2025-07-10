6f2e655378bc92c004fc859f475827f98d191e82
cpceventscan <cpcevents135@gmail.com>
2025-07-11 03:41:24 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'mysql.hostinger.com',
   user: process.env.DB_USER || 'u704382877_cpcevents',
   password: process.env.DB_PASSWORD || 'un54agS3pYw@.EK',
   database: process.env.DB_NAME || 'u704382877_cpcevents',
