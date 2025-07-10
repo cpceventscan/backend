96f3728e4877ac62b2fdad295e6c9f2c0a8ae3e0
cpceventscan <cpcevents135@gmail.com>
2025-07-11 03:21:42 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'cpc-event',
+  user: process.env.DB_USER || 'u704382877_cpcevents',
+  password: process.env.DB_PASSWORD || 'un54agS3pYw@.EK',
+  database: process.env.DB_NAME || 'u704382877_cpcevents',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
