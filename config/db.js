3cfa3f56a4993171aa89f9adb015233cd82b5d1b
cpceventscan <cpcevents135@gmail.com>
2025-11-13 17:51:54 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'cpc-event',
+  host: process.env.DB_HOST || 'srv1858.hstgr.io',
+  user: process.env.DB_USER || 'u704382877_cpc',
+  password: process.env.DB_PASSWORD || 'CPCeventscan2005.',
+  database: process.env.DB_NAME || 'u704382877_cpcevent',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+});
+
+pool.on('connection', async (connection) => {
+  await connection.query("SET time_zone = '+08:00'");
 });
 
 module.exports = pool;
